6e0d77cd6b1aaa95847eda9de023c06871c14370
ErzeKawek <130212655+ErzeKawek@users.noreply.github.com>
2024-02-09 17:03:52 +0700

    AuritisLeavesBlock Now Uses FabricBlockSettings Instead of BCL

--- src/main/java/paulevs/edenring/blocks/AuritisLeavesBlock.java
+++ src/main/java/paulevs/edenring/blocks/AuritisLeavesBlock.java
@@ -1,12 +1,12 @@
 package paulevs.edenring.blocks;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import org.betterx.bclib.blocks.BaseLeavesBlock;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
 import paulevs.edenring.registries.EdenBlocks;
@@ -15,7 +15,7 @@ import paulevs.edenring.registries.EdenParticles;
 public class AuritisLeavesBlock extends BaseLeavesBlock {
 	
 	public AuritisLeavesBlock() {
-		super(EdenBlocks.AURITIS_SAPLING, BlockBehaviour.Properties.copy(Blocks.AZALEA_LEAVES).mapColor(MapColor.GOLD));
+		super(EdenBlocks.AURITIS_SAPLING, FabricBlockSettings.copyOf(Blocks.AZALEA_LEAVES).mapColor(MapColor.GOLD));
 	}
 
 	@Override
